698cb8879880f44e2920bf0260d3be6d64a35a35
남기정 <114844315+LuckyPlace@users.noreply.github.com>
2023-11-09 10:17:33 +0900

    first commit

--- infix_to_postfix.py
+++ infix_to_postfix.py
@@ -1,15 +1,16 @@
-def infix_to_postfix():
-<<<<<<< Updated upstream
-    print()
-    print("Hellow Wordld!")
-    print("Hellow Wordld!")
-    print("Hellow Wordld!")
-    print("Hellow Wordld!")
-    print("Hellow Wordld!")
-    print("Hellow Wordld!")
-    print("Hellow Wordld!")
-    print("Hellow Wordld!")
+def infix_to_postfix(expr):
+    postfix = []
+    stack = []
 
-=======
-    print() 
->>>>>>> Stashed changes
+    #음수 구현 아이디어 : 숫자가 아니면서 한 글자면 연산자로 판정, 그 외에는 피연산자(숫자)로 판정
+    for token in expr:
+        if token[0] in ['+', '-' ,'*']:
+            #stack의 top()에 있는 연산자의 우선순위가 token의 연산자보다 크거나 같다면
+            #postfix에 stack의 top()을 append하고 stack을 pop()한다. 그리고 stack에 token을 append한다.
+        else:
+            postfix.append(token)
+
+    while stack:
+        postfix.append(stack.pop())
+
+    return postfix
