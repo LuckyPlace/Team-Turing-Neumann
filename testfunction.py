2c9df390e4e95956c5070fea7dbb27a928ac68bb
user1032 <heulem3@gmail.com>
2023-11-14 16:27:24 +0900

    4

--- testfunction.py
+++ testfunction.py
@@ -1,30 +1,3 @@
-def op_priority(op):
-    if op in ['+', '-']:
-        return 1
-    elif op == '*':
-        return 2
-
-def infix_to_postfix(expr, stack, number, postfix):
-    for token in expr:
-        if(len(token) == 1):
-            if token[0] in ['+', '-' ,'*']:
-                while stack and op_priority(stack[-1]) >= op_priority(token):
-                    postfix.append(stack.pop())
-                stack.append(token)
-            else:
-                number.append(int(token))
-        else:
-            number.append(int(token))
-
-    while stack:
-        postfix.append(stack.pop())
-
-    return postfix
-
-def clear(number, postfix):
-    postfix.clear()
-    number.clear()
-
 def testfunction(number, postfix):
 
     print("error1", end = "") #error 메시지 출력
@@ -35,7 +8,7 @@ def testfunction(number, postfix):
     for i in range(1,10000):
         postfix.append('+')
     assert 50005000 == calc_postfix(postfix, number), "sum plus error"
-    print('\b''\b''\b''\b''\b''\b', end = "") #출력 
+    print('\b''\b''\b''\b''\b''\b', end = "") #출력
     
     print("error2", end = "")
     clear(number, postfix)
@@ -99,51 +72,3 @@ def testfunction(number, postfix):
 
 
     print("test complete, No abnormality found")
-
-
-def calc_postfix(postfix, number):
-    result = 0
-
-    for token in postfix:
-        second = number.pop()
-        first = number.pop()
-        op = token[0]
-        if op == '+':
-            result = first + second
-        elif op == '-':
-            result = first - second
-        elif op == '*':
-            result = first * second
-        number.append(result)
-        
-
-    return result
-
-
-if __name__ == "__main__":
-
-    postfix = []
-    stack = []
-    number = []
-    expr = []
-
-    while True :
-
-        testfunction(number, postfix)
-        break
-
-        temp = input()
-        temp2 = temp
-        if(temp == "1225"):
-            print("허허 메리 크리스마스")
-            break
-        if(temp == '='):
-            infix_to_postfix(expr, stack, number, postfix)
-            print("{:d}".format(calc_postfix(expr, number)))
-            break
-        if not temp in ['+', '-' ,'*']:
-            assert float(temp)%1 == 0, "Error"
-                
-        expr.append(temp)
-
-
