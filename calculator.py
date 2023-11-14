2599945a27303addd2e8871347614fcacce094d5
user1032 <heulem3@gmail.com>
2023-11-14 16:26:14 +0900

    3

--- calculator.py
+++ calculator.py
@@ -1,60 +1,76 @@
-postfix = []        # infix를 postfix로 바꾼 표현을 저장하는 list 선언
-stack = []          # stack으로 활용하기 위한 list 선언
-number = []         # 정수만 따로 int형으로 저장하기 위해 list 선언
-
-def op_priority(op):       # 연산자의 우선순위를 결정하는 함수
+def op_priority(op):
     if op in ['+', '-']:
         return 1
-    elif op == '*':        # '*' 연산자의 우선순위가 더 높으므로 1보다 더 큰 2를 리턴
+    elif op == '*':
         return 2
 
-def infix_to_postfix(expr): # infix를 postfix로 바꾸는 함수
+def infix_to_postfix(expr, stack, number, postfix):
     for token in expr:
-        if(len(token) == 1): #길이가 1인 경우 양의 정수이거나 연산자
-            if token[0] in ['+', '-' ,'*']:  # 연산자인 경우 연산자의 우선순위에 따라
+        if(len(token) == 1):
+            if token[0] in ['+', '-' ,'*']:
                 while stack and op_priority(stack[-1]) >= op_priority(token):
-                    postfix.append(stack.pop()) # 스택의 맨위에 있는 연산자가 더 크거나 같은 연산자일 때까지 stack에서 pop()한 것을 postfix list에 저장하고
-                stack.append(token) # stack에 현재 값을 넣어준다.
+                    postfix.append(stack.pop())
+                stack.append(token)
             else:
-                number.append(int(token))   # 양의 정수인 경우 int형으로 바꿔 number list에 저장한다.
+                number.append(int(token))
         else:
-            number.append(int(token))     # 음의 정수인 경우 int형으로 바꿔 number list에 저장한다.
+            number.append(int(token))
 
     while stack:
-        postfix.append(stack.pop())     # stack에 남아있는 연산자들을 postfix list에 추가한다.
+        postfix.append(stack.pop())
 
     return postfix
 
+def clear(number, postfix):
+    postfix.clear()
+    number.clear()
 
-def calc_postfix(expr):     # infix_to_postfix함수로부터 리턴된 postfix를 계산하는 함수
+
+
+
+def calc_postfix(postfix, number):
     result = 0
 
     for token in postfix:
         second = number.pop()
         first = number.pop()
         op = token[0]
-        if op == '+':       # +인 경우 계산
+        if op == '+':
             result = first + second
-        elif op == '-':     # -인 경우 계산
+        elif op == '-':
             result = first - second
-        elif op == '*':     # *인 경우 계산
+        elif op == '*':
             result = first * second
         number.append(result)
         
+
     return result
 
 
 if __name__ == "__main__":
 
+    postfix = []
+    stack = []
+    number = []
     expr = []
+
     while True :
+
+        testfunction(number, postfix)
+        break
+
         temp = input()
         temp2 = temp
-        if(temp == "1225"):     # 이스터에그 : 크리스마스인 1225를 입력하면 발생
+        if(temp == "1225"):
             print("허허 메리 크리스마스")
             break
-        if(temp == '='):        # '='가 입력된 경우 지금까지 입력된 expr을 함수에 전달해 계산
-            expr = infix_to_postfix(expr)
-            print("{:d}".format(calc_postfix(expr)))
+        if(temp == '='):
+            infix_to_postfix(expr, stack, number, postfix)
+            print("{:d}".format(calc_postfix(expr, number)))
             break
+        if not temp in ['+', '-' ,'*']:
+            assert float(temp)%1 == 0, "Error"
+                
         expr.append(temp)
+
+
