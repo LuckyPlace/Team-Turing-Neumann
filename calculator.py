3bce7cb11d73a9232f7b4022bb4d8072f00a955b
남기정 <114844315+LuckyPlace@users.noreply.github.com>
2023-11-14 18:42:05 +0900

    refactoring

--- calculator.py
+++ calculator.py
@@ -1,20 +1,16 @@
-import sys
-
 def op_priority(op):       # 연산자의 우선순위를 결정하는 함수
     if op in ['+', '-']:
         return 1
     elif op == '*':        # '*' 연산자의 우선순위가 더 높으므로 1보다 더 큰 2를 리턴
         return 2
 
-def infix_to_postfix(expr, stack, number, postfix): # infix를 postfix로 바꾸는 함수
+def infix_to_postfix(expr, postfix): # infix를 postfix로 바꾸는 함수
+    stack = []          # stack으로 활용하기 위한 list 선언
     for token in expr:
         if len(token) == 1 and token[0] in ['+', '-', '*']: #길이가 1인 경우 양의 정수이거나 연산자
-            #if token[0] in ['+', '-' ,'*']:  # 연산자인 경우 연산자의 우선순위에 따라
             while stack and op_priority(stack[-1]) >= op_priority(token):
                 postfix.append(stack.pop()) # 스택의 맨위에 있는 연산자가 더 크거나 같은 연산자일 때까지 stack에서 pop()한 것을 postfix list에 저장하고
             stack.append(token) # stack에 현재 값을 넣어준다.
-        #elif token[0]>=0 and token[0]<10: # 0에서 9 사이의 수인 경우
-            #postfix.append(int(token))   # 양의 정수인 경우 int형으로 바꿔 number list에 저장한다.
         else:   #나머지의 경우
             postfix.append(token)     # 음의 정수인 경우 int형으로 바꿔 number list에 저장한다.
 
@@ -48,8 +44,6 @@ def calc_postfix(postfix):     # infix_to_postfix함수로부터 리턴된 postf
 if __name__ == "__main__":
     expr = []           # 식을 저장하기 위해 list 선언
     postfix = []        # infix를 postfix로 바꾼 표현을 저장하는 list 선언
-    stack = []          # stack으로 활용하기 위한 list 선언
-    number = []         # 정수만 따로 int형으로 저장하기 위해 list 선언
     while True :
         try:
             temp = input()
@@ -57,7 +51,10 @@ if __name__ == "__main__":
                 print("허허 메리 크리스마스")
                 break
             if(temp == '='):        # '='가 입력된 경우 지금까지 입력된 expr을 함수에 전달해 계산
-                infix_to_postfix(expr, stack, number, postfix)
+                if not expr:       # 아무것도 입력하지 않고 '='를 입력한 경우
+                    print("ERROR!")
+                    break
+                infix_to_postfix(expr, postfix)
                 print("{:d}".format(calc_postfix(postfix)))
                 break
             expr.append(temp)
